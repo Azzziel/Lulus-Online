018feb625c2e051b75f2a6c22ce05e77bc9d4032
Ivan Kurnia <ivan.kurnia.s@gmail.com>
2020-06-07 08:32:16 +0700

    Patched insert_node_status again, less rows

--- IT Infrastructure/Database/New Database/PHP/insert_node_status.php
+++ IT Infrastructure/Database/New Database/PHP/insert_node_status.php
@@ -46,28 +46,26 @@ $current_time_stamp = date("Y-m-d H:i:s", $current_unix_stamp);
 if ($previous_n_stats == $n_stats) {
     echo 'SUCCESS';
 } else {
-    if ($n_stats == 0) {
-        if (isset($previous_t_stamp)) {
-            $previous_unix_stamp = strtotime($previous_t_stamp);
-            $s_spent = $current_unix_stamp - $previous_unix_stamp;
+    if ($n_stats == 0 && isset($previous_t_stamp)) {
+        $previous_unix_stamp = strtotime($previous_t_stamp);
+        $s_spent = $current_unix_stamp - $previous_unix_stamp;
 
-            $query = "
-            INSERT INTO `node_summaries` 
-                        (`lc_id`, 
-                         `node_id`, 
-                         `s_spent`, 
-                         `t_in`, 
-                         `t_out`) 
-            VALUES      ($lc_id, 
-                         '$node_id', 
-                         $s_spent, 
-                         '$previous_t_stamp', 
-                         '$current_time_stamp')";
+        $query = "
+        INSERT INTO `node_summaries` 
+                    (`lc_id`, 
+                     `node_id`, 
+                     `s_spent`, 
+                     `t_in`, 
+                     `t_out`) 
+        VALUES      ($lc_id, 
+                     '$node_id', 
+                     $s_spent, 
+                     '$previous_t_stamp', 
+                     '$current_time_stamp')";
 
-            if (!($result = $mysqli->query($query))) {
-                echo 'FAILED';
-                exit();
-            }
+        if (!($result = $mysqli->query($query))) {
+            echo 'FAILED';
+            exit();
         }
     }
 
